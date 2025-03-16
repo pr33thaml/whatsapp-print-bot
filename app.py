7540b9db66f4e262247866d50c5e8e87eb7cd116
Preetham L <133993870+pr33thaml@users.noreply.github.com>
2025-03-16 18:16:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,5 @@ def webhook():
     return "OK", 200
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Render requires a dynamic port
+    app.run(host="0.0.0.0", port=port, debug=True)
